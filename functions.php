980843beeab03571cb82582bedee30116fa15102
katherineshaw <kshaw004@seattlecentral.edu>
2016-06-12 22:59:55 -0700

    A5

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Theme Name: Spring 2016 Web 170
+Author: Katherine Shaw
+Author URL: http://katherineshawdesign.com
+Description: Katherine is toooootally doing her homework, Mike.
+Version: 0.1
+*/
+
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<!-- Main Page Content -->        
+	 <img src="images/storefront.jpg" alt="Biscuit Bitch Downtown Storefront" height="80%" width="90%">
+        
+    <h3>Check out the Process, Bitches!</h3>
+        <div class="figure">
+            
+    	  <figure>
+		    <img src="images/biscuits1.jpg" alt="Biscuit Photo" title="Fresh Biscuits" />
+		  </figure>
+            
+		  <figure>
+		    <img src="images/biscuits.jpg" alt="Biscuits" title="Biscuit Bitch Pike Storefront" />
+		  </figure>
+            
+        </div>
+    <h3>#biscuitbitch when you come in, and your pic could show up here!</h3>
+<!-- INSTANSIVE WIDGET --><script src="//instansive.com/widget/js/instansive.js"></script><iframe src="//instansive.com/widgets/8e34a79a5c19bd2505fcfa7a8a5eaa19e358a043.html" id="instansive_8e34a79a5c" name="instansive_8e34a79a5c"  scrolling="no" allowtransparency="true" class="instansive-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+
+<?php get_footer(); ?>
