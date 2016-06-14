eb6994a70e4b8ed2e1502b26c1417eacaa5034c2
katherineshaw <kshaw004@seattlecentral.edu>
2016-06-14 15:29:10 -0700

    A5 Includes

--- footer.php
+++ footer.php
@@ -5,7 +5,10 @@
    &copy; 2015, All Rights Reserved ~ Authored by Katherine Shaw~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a>
    </small>
   </footer>
-        
-    </body>
-    
+
+<!-- WP footer -->
+<?php wp_footer(); ?>
+<!-- WP footer ENDS -->
+
+</body>    
 </html>
