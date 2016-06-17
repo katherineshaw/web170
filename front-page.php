1c4ff8874c3b1e6fde1b19bc913a8c7531826051
katherineshaw <kshaw004@seattlecentral.edu>
2016-06-17 12:08:43 -0700

    A7 Front Page

---
+++ front-page.php
@@ -0,0 +1,19 @@
+<!-- A7 Page.php -->
+<?php get_header(); ?>
+
+<!-- Main Page Content -->        
+
+<div class="main-content">
+<?php if (have_posts() ) : while (have_posts() ) : the_post();// start loop ?> 
+
+<h2><a href="<?php the_permalink(); // link to page posting ?>"><?php the_title();// get page title ?></a></h2>
+<?php the_content(''); // get page content ?>
+<?php endwhile; endif; // end loop ?>
+<small>front-page.php</small>
+</div>
+
+<!-- A6 SIDEBAR -->
+<?php get_sidebar(); ?>
+<!-- SIDEBAR -->
+
+<?php get_footer(); ?>
