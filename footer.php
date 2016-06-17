10ea887a5cc532eb8745375d07a5d0982b3e3a12
katherineshaw <kshaw004@seattlecentral.edu>
2016-06-17 11:58:19 -0700

    A7

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
         
-        <footer>
+    <footer>
             
    <small>
-   &copy; 2015, All Rights Reserved ~ Authored by Katherine Shaw~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a>
+   &copy; 2016, All Rights Reserved ~ Authored by Katherine Shaw ~ <?php wp_loginout(); ?>
    </small>
   </footer>
 
