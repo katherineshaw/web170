5f673b0ff65309aa978f55c1233efd04754b7b5e
katherineshaw <kshaw004@seattlecentral.edu>
2016-06-20 02:02:02 -0700

    A9

--- functions.php
+++ functions.php
@@ -8,4 +8,17 @@ Description: Katherine is toooootally doing her homework, Mike.
 Version: 0.1
 */
 
+// My Menus A6
+register_nav_menus(array(
+    'navigation' => __('Main'),));
+
+// My Sidebar A6
+//register_sidebar 
+
+// Make Thumbnails A9
+add_theme_support('post-thumbnails');
+
+// Page Excerpts A9
+add_post_type_support ('page', 'excerpt'); 
+
 ?>
