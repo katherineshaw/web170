b5b5f3e7722950c76fbba2eb80ea5939718c9fa5
katherineshaw <kshaw004@seattlecentral.edu>
2016-06-20 02:01:19 -0700

    A8

--- single.php
+++ single.php
@@ -4,9 +4,9 @@
 <div class="main-content">
 <?php if (have_posts() ) : while (have_posts() ) : the_post();// start loop ?> 
 <div id="posting">
-<h2><a href="<?php the_permalink(); // link to page posting ?>"><?php the_title();// get page title ?></a></h2>
-<small>Posted <?php the_time ('F j, Y'); ?> by <?php the_author_posts_link; ?></small>
-<?php the_post_thumbnail('large'); ?>
+<h2><a href="<?php the_title(); // get posting title ?>"></a></h2>
+<small>Posted on <?php the_time ('F j, Y'); ?> by <?php the_author(); in <?php the_category(', '); ?></small>
+<?php the_post_thumbnail('large'); // get image ?>
 <?php the_content(''); // get page content ?>
 <?php endwhile; endif; // end loop ?>
 <small>single.php</small>
