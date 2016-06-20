67c28fc64fd5a6408748ffdd5910caa75e95d1c0
katherineshaw <kshaw004@seattlecentral.edu>
2016-06-20 02:04:15 -0700

    A9

--- index.php
+++ index.php
@@ -1,28 +1,25 @@
 <?php get_header(); ?>
-<title><?php bloginfo('description'); ?> // <?php bloginfo('name'); ?></title>
-<!-- Main Page Content -->        
-	 <img src="images/storefront.jpg" alt="Biscuit Bitch Downtown Storefront" height="80%" width="90%">
+<title><?php bloginfo('description'); ?><?php bloginfo('name'); ?></title>
+<!-- Content -->        
 
-<div class="first_Index">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
-<h2><a href="<?php the_permalink(); // link to the page or posting ?>"><?php the_title(); // get the page or posting title ?></a></h2>
-<?php the_content(''); // get page or posting written content ?>
-<?php endwhile; endif; // end the loop ?>
+<div class="main-content">
+<?php if ( have_posts()) : while ( have_posts()) : the_post();// start loop ?> 
+    
+    <article class="post-excerpt">
+<h4><a href="<?php the_permalink(); // link to the page ?>"><?php the_title();// get page title ?></a></h4>
+<small>Posted on <?php the_time('F j, Y'); //retrieve time ?> by <?php the_author(); //retrieve author ?> in <?php the_category(', '); //retrieve category ?></small>
+<a href="<?php the_permalink(); //link to posting ?>"> 
+<?php the_post_thumbnail('thumbnail'); //get thumbnail ?></a>    
+<?php the_excerpt(''); // get excerpt from post ?>
+        <p class="read-more"><a href="<?php the_permalink(); //link to FULL posting via read more tag ?>">Read More!</a></p>
+    </article>
+<?php endwhile; endif; // end loop ?>
+<small>index.php</small>
+
 </div>
         
-    <h3>Check out the Process, Bitches!</h3>
-        <div class="figure">
-            
-    	  <figure>
-		    <img src="images/biscuits1.jpg" alt="Biscuit Photo" title="Fresh Biscuits" />
-		  </figure>
-            
-		  <figure>
-		    <img src="images/biscuits.jpg" alt="Biscuits" title="Biscuit Bitch Pike Storefront" />
-		  </figure>
-            
-        </div>
-    <h3>#biscuitbitch when you come in, and your pic could show up here!</h3>
-<!-- INSTANSIVE WIDGET --><script src="//instansive.com/widget/js/instansive.js"></script><iframe src="//instansive.com/widgets/8e34a79a5c19bd2505fcfa7a8a5eaa19e358a043.html" id="instansive_8e34a79a5c" name="instansive_8e34a79a5c"  scrolling="no" allowtransparency="true" class="instansive-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+<!-- A6 SIDEBAR -->
+<?php get_sidebar(); ?>
+<!-- SIDEBAR -->
 
 <?php get_footer(); ?>
